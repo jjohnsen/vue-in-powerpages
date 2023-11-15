104c0edbd8df7205e4aadc84d1913a1b2357429f
Jan-Åge Johnsen <jan-age.johnsen@evidi.com>
2023-11-15 22:54:20 +0100

    Added step 2

--- vue-in-powerpages/vite.config.js
+++ vue-in-powerpages/vite.config.js
@@ -1,12 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: true
+  },
   plugins: [
     vue(),
+    basicSsl()
   ],
   resolve: {
     alias: {
